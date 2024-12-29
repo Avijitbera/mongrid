30b0e2c58a4df48bb73a01580f8ac53b25de6bc7
Avijit bera <avijit.bera@fitnearn.com>
2024-12-29 23:37:02 +0530

    added validator

--- src/schema.ts
+++ src/schema.ts
@@ -3,4 +3,34 @@ import { SchemaType } from './types/types'
 
 export class Schema<T extends SchemaType>{
     constructor(private schema: T){}
+
+    getSchema():T{
+        return this.schema
+    }
+
+    validate(data:any):boolean {
+        for(const [field, config] of Object.entries(this.schema)){
+            //Required field validation
+            if(config.required && data[field] === undefined || data[field] === null){
+                throw new Error(`${field} is required`)
+            }
+            //Type validation
+            if(data[field] !== undefined){
+                const value = data[field]
+                const type = config.type
+                if(!this.validateType(value, type)){
+                    throw new  Error(`${field} should be of type ${type}`)
+                }
+                //Custom validation
+                if(config.validate && !config.validate(value)){
+                    throw new Error(`${field} failed custom validation`)
+                }
+            }
+        }
+        return true;
+    }
+
+    private validateType(value:any, type:any): boolean{
+        return true
+    }
 }
