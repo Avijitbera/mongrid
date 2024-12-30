25cd63ab2c1d48298eb285b7d426b6880e2ae582
Avijitbera <avijitbera775@gmail.com>
2024-12-30 10:41:30 +0530

    created buildQuery function on query builder

---
+++ src/model/base.ts
@@ -0,0 +1,6 @@
+import {} from 'mongodb'
+import { SchemaType } from '../types/types';
+
+export class BaseModel<T extends SchemaType> {
+    
+}
--- src/model/query_builder.ts
+++ src/model/query_builder.ts
@@ -1,4 +1,16 @@
 import {SchemaType} from '../types/types'
-import {} from '../types/model'
+import {ModelDocument, ModelQueryOptions} from '../types/model'
+import { BaseModel } from './base'
+import { Filter } from 'mongodb'
+
+export class QueryBuilder<T extends SchemaType> {
+    constructor(private model: BaseModel<T>){}
+
+    buildQuery(filter: Filter<ModelDocument<T>> = {}):Filter<ModelDocument<T>> {
+        return {
+            ...filter,
+        }
+    }
+} 
 
 
