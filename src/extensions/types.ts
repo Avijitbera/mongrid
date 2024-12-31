213cc729f0f40bef82c6055a888c3172a2053004
Avijitbera <avijitbera775@gmail.com>
2024-12-31 08:31:45 +0530

    completed extension type

--- src/extensions/types.ts
+++ src/extensions/types.ts
@@ -9,5 +9,20 @@ export interface ExtensionContext<T extends SchemaType = SchemaType> {
     
 }
 
-export interface QueryExtension {}
+export interface QueryExtension {
+    beforeFind?: (filter: Filter<Document>, options?:FindOptions) => Promise<[Filter<Document>, FindOptions?]>;
+    afterFind?: (result: Document[]) => Promise<Document[]>;
+    beforeCreate?: (data:any) => Promise<any>;
+    afterCreate?: (result: Document) => Promise<Document>;
+    beforeUpdate?:(filter: Filter<Document>, update: any) => Promise<[Filter<Document>, any]>;
+    afterUpdate?: (result: Document | null) => Promise<Document | null>;
+  beforeDelete?: (filter: Filter<Document>) => Promise<Filter<Document>>;
+  afterDelete?: (success: boolean) => Promise<boolean>;
 
+}
+
+export interface ModelExtension {
+    methods?: Record<string, (this: Model<any>, ...args:any[]) => any>;
+    statics?: Record<string, (...args:any[]) => any>;
+    queryExtensions?: QueryExtension;
+}
