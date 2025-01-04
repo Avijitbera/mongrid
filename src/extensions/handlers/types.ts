89d75bfab5e45be4e87b23a55ba59ee2c887ac40
Avijitbera <avijitbera775@gmail.com>
2025-01-04 07:57:52 +0530

    created collection file

--- src/extensions/handlers/types.ts
+++
@@ -1,9 +0,0 @@
-import { Document } from "mongodb";
-import { QueryHandlers } from "../types";
-
-export type HandlerResult<T> = T | [T] | [T, T];
-export interface HandlerContext<T extends Document = Document> {
-    handler: QueryHandlers[keyof QueryHandlers];
-    args: unknown[];
-    phase: keyof QueryHandlers;
-}
