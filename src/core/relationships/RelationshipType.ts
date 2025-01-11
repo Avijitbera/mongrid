c0b47b2c7ea5a1cf3f53b31192312f276632d53b
Avijitbera <avijitbera775@gmail.com>
2025-01-11 23:03:27 +0530

    added RelationshipMetadata

--- src/core/relationships/RelationshipType.ts
+++ src/core/relationships/RelationshipType.ts
@@ -1,5 +1,19 @@
+import { Document } from "mongodb";
+import { Model } from "../model";
+
 export enum RelationshipType {
     OneToOne = 'OneToOne',
     OneToMany = 'OneToMany',
     ManyToMany = 'ManyToMany',
+}
+
+export class RelationshipMetadata<T extends Document, R extends Document>{
+    constructor(
+        public type: RelationshipType,
+        public relatedModel: Model<R>,
+        public foreignKey: string,
+        public casecade: boolean = false,
+        public bidirectional: boolean = false,
+        public inverseField?: string
+    ){}
 }
