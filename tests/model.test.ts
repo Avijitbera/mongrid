8186779ae1e16b1e317d6b4803cb57bcfcf24b40
Avijitbera <avijitbera775@gmail.com>
2025-01-22 20:56:20 +0530

    created user model

--- tests/model.test.ts
+++ tests/model.test.ts
@@ -1,4 +1,5 @@
-import {Database, Model} from '../src'
+import {cleanup, connect} from './db'
+import {Database, FieldBuilder, Model} from '../src'
 interface User {
     id: string;
     name: string;
@@ -9,5 +10,18 @@ describe('Model - Save and Get', () =>{
     let db: Database;
     let userModel: Model<User>;
 
-    
+    beforeAll(async() =>{
+        const mongodb = await connect()
+        db = new Database(mongodb)
+
+        userModel = new Model<User>(db, "users")
+            .addField("id", new FieldBuilder<string>("id").type(String).required().build())
+            .addField("name", new FieldBuilder<string>("name").type(String).required().build())
+            .addField("age", new FieldBuilder<number>("age").type(Number).build());
+
+    })
+
+    afterAll(async () => {
+        await cleanup();
+    });
 })
