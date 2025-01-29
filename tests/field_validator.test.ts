19031c35cab641d318f07c1260d43ea160334147
Avijitbera <avijitbera775@gmail.com>
2025-01-29 21:53:27 +0530

    created beforeall function

--- tests/field_validator.test.ts
+++ tests/field_validator.test.ts
@@ -1,5 +1,7 @@
 import dotenv from 'dotenv';
-
+import { Database } from '../src/core/Database';
+import { Model, FieldBuilder } from '../src';
+import {connect} from './db'
 dotenv.config();
 
 interface Product {
@@ -8,3 +10,20 @@ interface Product {
     price: number;
     quantity: number;
 }
+
+describe('Field Validation Tests', () => {
+    let db: Database;
+    let productModel: Model<Product>;
+
+    beforeAll(async () => {
+        const mongodb = await connect();
+        db = new Database(mongodb);
+
+        productModel = new Model<Product>(db, "products")
+            .addField("id", new FieldBuilder<string>("id").type(String).required().build())
+            .addField("name", new FieldBuilder<string>("name").type(String).required().build())
+            .addField("price", new FieldBuilder<number>("price").type(Number).required().build())
+            .addField("quantity", new FieldBuilder<number>("quantity").type(Number).required().build());
+    });
+
+})
