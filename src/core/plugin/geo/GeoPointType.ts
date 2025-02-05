2f1082f51634776fced1acdcb3f410eecc75a342
Avijitbera <avijitbera775@gmail.com>
2025-02-05 22:39:10 +0530

    implmented validate, trasform function

--- src/core/plugin/geo/GeoPointType.ts
+++ src/core/plugin/geo/GeoPointType.ts
@@ -1,3 +1,4 @@
+import { CustomType } from "../../types/CustomType";
 
 
 export interface GeoPoint {
@@ -5,3 +6,32 @@ export interface GeoPoint {
     coordinates: [number, number]; // [longitude, latitude]
 }
 
+export class GeoPointType implements CustomType<GeoPoint>{
+    validate(value: GeoPoint): void {
+        if (!value.type || value.type !== "Point" || !Array.isArray(value.coordinates) || value.coordinates.length !== 2) {
+            throw new Error("Invalid GeoPoint format");
+        }
+    }
+    trasform(value: GeoPoint): GeoPoint {
+        return {
+            type: "Point",
+            coordinates: value.coordinates,
+        };
+    }
+    toMongoSchema() {
+        return {
+            bsonType: "object",
+            required: ["type", "coordinates"],
+            properties: {
+                type: { bsonType: "string", enum: ["Point"] },
+                coordinates: {
+                    bsonType: "array",
+                    items: { bsonType: "double" },
+                    minItems: 2,
+                    maxItems: 2,
+                },
+            },
+        };
+    }
+
+}
