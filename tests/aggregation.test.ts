4a765f8bbaadca8858532042bab569c5b2e6dfb3
Avijitbera <avijitbera775@gmail.com>
2025-02-15 23:03:19 +0530

    created beforeAll function for unit test

--- tests/aggregation.test.ts
+++ tests/aggregation.test.ts
@@ -1,9 +1,21 @@
-import { Model } from '../src';
+
+import { FieldBuilder, Model } from '../src';
 import { Database } from '../src/core/Database';
 import {User} from '../tests/model/user.model'
+import { connect } from './db';
 
 
 describe("Type-Safe Aggregation Tests", () => {
     let db: Database;
     let userModel: Model<User>;
+
+    beforeAll(async () => {
+        const mongodb = await connect();
+        db = new Database(mongodb);
+
+        userModel = new Model<User>(db, "users")
+            .addField("id", new FieldBuilder<string>("id").type(String).required().build())
+            .addField("name", new FieldBuilder<string>("name").type(String).required().build())
+            .addField("age", new FieldBuilder<number>("age").type(Number).build());
+    });
 })
