8668b9ec91cb242710169322ced5cbcca922c168
Avijitbera <avijitbera775@gmail.com>
2025-02-15 23:01:32 +0530

    creating aggregation test files

---
+++ tests/aggregation.test.ts
@@ -0,0 +1,9 @@
+import { Model } from '../src';
+import { Database } from '../src/core/Database';
+import {User} from '../tests/model/user.model'
+
+
+describe("Type-Safe Aggregation Tests", () => {
+    let db: Database;
+    let userModel: Model<User>;
+})
