9943a2d907c2598b73dbf991eaef99b80734edce
Avijitbera <avijitbera775@gmail.com>
2025-02-27 23:36:24 +0530

    created beforeAll relationship test

--- tests/relationship.test.ts
+++ tests/relationship.test.ts
@@ -1,7 +1,8 @@
 import { ObjectId } from "mongodb";
-import { Database, Model } from "../src";
+import { Database, FieldBuilder, Model } from "../src";
 
-
+import {connect} from './db'
+import { RelationshipMetadata, RelationshipType } from "../src/core/relationships/RelationshipType";
 
 interface User {
     id: string;
@@ -20,4 +21,32 @@ describe("Relationship Tests", () => {
     let db: Database;
     let userModel: Model<User>;
     let postModel: Model<Post>;
+
+    beforeAll(async () =>{
+        const mongodb = await connect();
+        db = new Database(mongodb);
+
+        // Define User model
+        userModel = new Model<User>(db, "users")
+            .addField("id", new FieldBuilder<string>("id").type(String).required().build())
+            .addField("name", new FieldBuilder<string>("name").type(String).required().build());
+
+        // Define Post model
+        postModel = new Model<Post>(db, "posts")
+            .addField("id", new FieldBuilder<string>("id").type(String).required().build())
+            .addField("title", new FieldBuilder<string>("title").type(String).required().build())
+            .addField("userId", new FieldBuilder<ObjectId>("userId").type(ObjectId).required().build());
+   
+            userModel.addRelationship("posts", new RelationshipMetadata(
+                RelationshipType.OneToMany,
+                postModel,
+                "userId" // Foreign key in the Post model
+            ));
+    
+            postModel.addRelationship("user", new RelationshipMetadata(
+                RelationshipType.OneToOne,
+                userModel,
+                "userId" // Foreign key in the Post model
+            ));
+        })
 })
